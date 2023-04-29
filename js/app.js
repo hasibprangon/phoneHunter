e27f0413c2cf18484bfc69ab4ec351a8606c7611
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-29 16:35:50 +0600

    enter event handler

--- js/app.js
+++ js/app.js
@@ -33,13 +33,12 @@ const displayPhones = (phones, dataLimit) => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col')
         phoneDiv.innerHTML = `
-      <div class="card h-100 p-3">
-      <img class="" src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card h-100 p-3">
+       <img class="" src="${phone.image}" class="card-img-top" alt="...">
       <div class="card-body">
-          <h5 class="card-title">${phone.phone_name
-            }</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
       </div>
   </div>
       `;
@@ -57,8 +56,19 @@ const processSearch = (dataLimit) => {
 }
 
 document.getElementById('btn-search').addEventListener('click', function () {
+    // start loader
     processSearch(10);
 });
+
+// search input field presing  enter kye
+// enter kye event listener always happend on input field not on btn
+const input = document.getElementById('searchField');
+input.addEventListener('keydown', function(event) {
+    console.log(event.key);
+  if (event.key === 'Enter') {
+    processSearch(10);
+  }
+})
 const togglrLoader = isLoading => {
     const spinnerSection = document.getElementById('loader');
     if (isLoading) {
@@ -72,4 +82,11 @@ const togglrLoader = isLoading => {
 document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 });
+
+const loadPhoneDetails = async(id) => {
+const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+const res = await fetch(url);
+const data = await res.json();
+console.log(data);
+};
 loadPhone('iphone');
