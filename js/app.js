d98912181d44b097b0b748c9c7039689d2c7b07e
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-29 19:47:49 +0600

    Show Phone Details data on a Modal

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const loadPhone = async (searchText, dataLimit) => {
 };
 
 const displayPhones = (phones, dataLimit) => {
-    console.log(phones);
+    // console.log(phones);
     const phoneContainer = document.getElementById('phoneContainer');
     phoneContainer.textContent = '';
     //display 10 phones only
@@ -38,7 +38,8 @@ const displayPhones = (phones, dataLimit) => {
       <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
+          <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+          
       </div>
   </div>
       `;
@@ -63,11 +64,11 @@ document.getElementById('btn-search').addEventListener('click', function () {
 // search input field presing  enter kye
 // enter kye event listener always happend on input field not on btn
 const input = document.getElementById('searchField');
-input.addEventListener('keydown', function(event) {
-    console.log(event.key);
-  if (event.key === 'Enter') {
-    processSearch(10);
-  }
+input.addEventListener('keydown', function (event) {
+    // console.log(event.key);
+    if (event.key === 'Enter') {
+        processSearch(10);
+    }
 })
 const togglrLoader = isLoading => {
     const spinnerSection = document.getElementById('loader');
@@ -83,10 +84,21 @@ document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 });
 
-const loadPhoneDetails = async(id) => {
-const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
-const res = await fetch(url);
-const data = await res.json();
-console.log(data);
+const loadPhoneDetails = async (id) => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetail(data);
 };
+
+const displayPhoneDetail = phone => {
+console.log(phone);
+const modalTitle = document.getElementById('phoneDetailModalLabel')
+modalTitle.innerText = phone.data.name;
+const phoneDetailsModal = document.getElementById('phone-detail-modal-body');
+phoneDetailsModal.innerHTML =`
+<p> Release Date: ${phone.data.releaseDate ? phone.data.releaseDate : 'No release date found'}</p>
+<p> Others: <br> WLAN: ${phone.data.others ? phone.data.others.WLAN : 'No data found'} ,<br> Bluetooth: ${phone.data.others ? phone.data.others.Bluetooth : 'No data found'} , <br> GPS: ${phone.data.others ? phone.data.others.GPS : 'No data found'}</p>
+` ;
+}
 loadPhone('iphone');
